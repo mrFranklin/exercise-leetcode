132e66788efe56b57dc5e19ae27274a9f4ac61ee
Franklin <mr_franklin@126.com>
2017-08-24 21:18:35 +0800

    refactor AddTwoNumbers of cpp version

--- 002-AddTwoNumbers/Solution.cpp
+++ 002-AddTwoNumbers/Solution.cpp
@@ -2,7 +2,7 @@
 * @Author: Franklin
 * @Date:   2017-08-24 17:24:30
 * @Last Modified by:   Franklin
-* @Last Modified time: 2017-08-24 20:49:12
+* @Last Modified time: 2017-08-24 21:17:21
 */
 
 /*
@@ -27,44 +27,28 @@ struct ListNode {
 };
 
 class Solution {
-private:
-    void setNextDataAndMove(ListNode **header, ListNode **p, int data) {
-        ListNode *node = new ListNode(data);
-        if ((*header) == NULL) {
-            *header = node;
-            (*p) = node;
-        } else {
-            (*p)->next = node;
-            (*p) = (*p)->next;
-        }
-    }
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* lRet = NULL;
-        ListNode* p = lRet;
+        ListNode* dummy = new ListNode(0);
+        ListNode* curNode = dummy;
         int carry = 0;
         int data;
         while(l1 != NULL || l2 != NULL) {
-            if (l1 == NULL) {
-                data = l2->val + carry;
-                l2 = l2->next;
-            } else if (l2 == NULL) {
-                data = l1->val + carry;
-                l1 = l1->next; 
-            } else {
-                data = l1->val + l2->val + carry;
-                l1 = l1->next;
-                l2 = l2->next; 
-            }
-            setNextDataAndMove(&lRet, &p, data % 10);
+            int x1 = (l1 != NULL) ? l1->val : 0;
+            int x2 = (l2 != NULL) ? l2->val : 0;
+            data = x1 + x2 + carry;
+
+            curNode->next = new ListNode(data % 10);
+            curNode = curNode->next;
+
             carry = data / 10;
+            if (l1 != NULL) l1 = l1->next;
+            if (l2 != NULL) l2 = l2->next;
         }
-
-        if (carry != 0) {
-            setNextDataAndMove(&lRet, &p, carry);
+        if (carry > 0) {
+            curNode->next = new ListNode(carry);
         }
-
-        return lRet;
+        return dummy->next;
     }
 };
 
